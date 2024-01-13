03333ddaa8ea3121ed2313c9cdcad9cf99ef4a14
nolythedev <nolychigwedere@hotmail.co.uk>
2024-01-13 15:34:24 +0000

    startQuiz event listener now hides start-screen and shows questions screen in place

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,6 +4,10 @@ import { questions } from "./questions.js";
 var startButton = document.querySelector('#start');
 // Grabs time element
 var timerEl = document.querySelector('#time');
+// Grabs start-screen div 
+var startScreen = document.querySelector('#start-screen');
+// Grabs question div 
+var questionScreen = document.querySelector('#questions');
 
 questions;
 
@@ -14,12 +18,12 @@ console.log(questions[0].question);
 // }
 
 
-// When start button is clicked timer starts 
 
 // Function for timer
 function timer() {
-    var timeLeft = 10;
-
+    var timeLeft = 60;
+    
+    // When start button is clicked timer starts 
     var timerInterval = setInterval(function () {
         timeLeft--;
         timerEl.textContent = timeLeft;
@@ -31,12 +35,14 @@ function timer() {
     }, 1000);
 }
 
-// When start button is clicked start screen class is = to hide
-
-// When start button is clicked #questions screen = show
 function startQuiz(event) {
     console.log('Start has been clicked and timer has started');
     timer();
+    
+    // When start button is clicked start screen class is = to hide
+    startScreen.className = 'start hide';
+    // When start button is clicked #questions screen = show
+    questionScreen.classList.remove('hide');
 }
 
 startButton.addEventListener('click', startQuiz)
